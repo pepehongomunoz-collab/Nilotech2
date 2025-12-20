a52c8156bba6210ecd972b78ead7d41b84753e61
Nicolas Muñoz <pepehongomunoz@gmail.com>
2025-12-20 20:27:38 -0300

    Configure custom domain nilotech.online

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nilotech2/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
